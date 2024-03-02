54878d5d716610c31cc91d24fed25cb39090167b
Raluca <andreearalucadavid@gmail.com>
2024-03-02 19:49:20 +0200

    Add functionality to add a new category.

--- Dictionary/Dictionary/AddWord.xaml.cs
+++ Dictionary/Dictionary/AddWord.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +21,11 @@ namespace Dictionary
     /// </summary>
     public partial class AddWord : Page
     {
+        private WordsManager data = new WordsManager();
         public AddWord()
         {
             InitializeComponent();
+            data.LoadWords();
         }
         private void BackClick(object sender, RoutedEventArgs e)
         {
@@ -34,10 +37,40 @@ namespace Dictionary
         }
         private void FinishClick(object sender, RoutedEventArgs e)
         {
+            string name = wordText.Text;
+            string description = descriptionText.Text;
             NavigationService navigationService = NavigationService.GetNavigationService(this);
             if (navigationService != null)
             {
                 navigationService.Navigate(new Uri("Admin.xaml", UriKind.Relative));
+            }
+        }
+        private void AddCategories(object sender, RoutedEventArgs e)
+        {
+            categoryComboBox.Items.Clear();
+            categoryComboBox.Items.Add("Add category");
+            foreach (string category in data.Categories)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = category;
+                categoryComboBox.Items.Add(item);
+            }
+            categoryComboBox.SelectedIndex = -1;
+        }
+        private void categorySelection(object sender, SelectionChangedEventArgs e)
+        {
+            if (categoryComboBox.SelectedItem != null && categoryComboBox.SelectedItem.ToString() == "Add category")
+            {
+                string newCategory = Microsoft.VisualBasic.Interaction.InputBox("Add the new category:", "Add category", "");
+                if (!string.IsNullOrEmpty(newCategory))
+                {
+                    categoryComboBox.Items.Insert(categoryComboBox.Items.Count - 1, newCategory);
+                    categoryComboBox.SelectedItem = newCategory;
+                }
+                else
+                {
+                    categoryComboBox.SelectedIndex = -1;
+                }
             }
         }
     }
